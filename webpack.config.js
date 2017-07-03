52fac6840839ab1823ab99247f5b473d4c6456ab
Guilherme Milfont Moreno <guilherme.milfont@gmail.com>
2017-07-03 15:43:47 -0300

    alterado title no HtmlWebapckPlugin no webpack

--- webpack.config.js
+++ webpack.config.js
@@ -54,7 +54,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
-      title: 'Red Venture',
+      title: 'Boiler',
       template: path.resolve(__dirname, './src/index.html'),
       filename: 'index.html'
     }),
